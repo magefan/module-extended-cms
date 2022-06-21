ca1632cf119d231ff66f8185fd65460fe49c2f04
DenysTsymbal <denys.tsymbal@magefan.com>
2022-06-21 13:36:57 +0300

    Add module

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magefan_ExtendedCms', __DIR__);
+
